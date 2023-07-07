b85d509d78280f21cd10928bafd8f29541046b87
Akos Arva <arva23@gmail.com>
2023-07-07 17:37:44 +0200

    Moved iterative model constraint into genmath lib

---
+++ Persephone/genmath/IterativeModel.cpp
@@ -0,0 +1,25 @@
+#include "IterativeModel.h"
+
+template <class FT, class T>
+genmath::IterativeModel<FT, T>::IterativeModel() {
+
+
+}
+
+template <class FT, class T>
+genmath::IterativeModel<FT, T>::IterativeModel(const IterativeModel& orig) {
+
+
+}
+
+template <class FT, class T>
+genmath::IterativeModel<FT, T>::~IterativeModel() {
+
+
+}
+
+template <class FT, class T>
+genmath::IterativeModel<FT, T>& genmath::IterativeModel<FT, T>::operator=(const IterativeModel<FT, T>& orig) {
+
+	return *this;
+}
